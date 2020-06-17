3f222b6cf6e2638c339bca509cb52e80d959d422
Japefuloni <japefuloni@gmail.com>
2020-06-17 11:25:20 -0500

    japefuloni

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 Route::get('formulario/inactivar', 'FormularioController@inactivar')->name('formulario.inactivar')->middleware('auth');
 Route::get('formulario/{id}/updateinac', 'FormularioController@updateinac')->name('formulario.updateinac')->middleware('auth');
 Route::view('/','home', ['nombre'=>'Jairo Peña Fuentes'])->name('home');
+Route::get('home/externos', 'Externos\ExternosController@homeext')->name('homeext');
 
 Route::view('inicio','inicio', ['nombre'=>'Jairo Peña Fuentes'])->name('inicio');
 //Route::resource('sedes', 'SedesController')->names('sedes')->parameters(['sedes'  =>  'sedes'])->middleware('role:1|2');
@@ -27,6 +28,7 @@ Route::resource('formulario', 'FormularioController')->names('formulario')->para
 Route::get('formulario/create/{n_idciudad}', 'FormularioController@listarSedesAjax')->name('sedes.listar');
 
 Route::resource('users', 'UsersController')->names('users')->parameters(['users'  =>  'users']);//->middleware('role:1|2');
+Route::resource('usersupb', 'Loginupb\UsersupbController')->names('usersupb')->parameters(['users'  =>  'users']);//->
 Route::resource('usuarios', 'UsuariosController')->names('usuarios')->parameters(['usuarios'  =>  'usuarios']);//->middleware('role:1|2');
 Route::get('usuarios/editar/{usuarios}', 'UsuariosController@editar')->name('usuarios.editar');
 Route::resource('sedes', 'SedesController')->names('sedes')->parameters(['sedes'  =>  'sedes'])->middleware('auth');;//->middleware('role:1|2');
@@ -39,20 +41,12 @@ Route::get('reportes/reporte2', 'ReportesController@reporte2')->name('reportes.r
 Route::get('reportes/reporte3', 'ReportesController@reporte3')->name('reportes.reporte3')->middleware('auth');
 Route::get('reportes/reporte4', 'ReportesController@reporte4')->name('reportes.reporte4')->middleware('auth');
 
+/* para despues del cambio Oracle */
 
-/* Route::get('losusuarios', function(){
-    return datatables()
-    ->query(DB::table('users'))
-    ->addColumn('action', function ($registro) {
-        return 
-        '
-        <a href="'.route('users.show', $registro->n_idusuario).'"> Ver</a>
+Route::get('/loginupb','Loginupb\LoginupbController@showLoginForm')->name('loginupb');
+Route::post('/autenticarupb','Loginupb\LoginupbController@validarLogin')->name('loginupb.validar');
 
-       ';
-    })
-    ->toJson();
 
-}); */
 Route::get('losusuarios', 'UsuariosController@getListaUsuarios')->name('losusuarios');
 Route::get('losformularios', 'FormularioController@getListaFormularios')->name('losformularios');
 Route::get('reporteador1', 'ReportesController@getReporte1Formularios')->name('reporteador1');
