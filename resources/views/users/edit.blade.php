6c1da6dd176b5856c21e81a42d76e32bc2f2496f
jmantillap <jmantillap@gmail.com>
2020-06-23 10:50:53 -0500

    Se actaliza para los combos dependientes

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -25,7 +25,30 @@
 @section('script-custom')
 <script>
     $(function () {      
-      $("#menuUsuario" ).addClass("active" );
-     });
+        $("#menuUsuario" ).addClass("active" );
+        $('#n_idciudad').change(function() {
+              $('#n_idsede').empty().append('<option value="" selected>--Seleccionar sede--</option>');            
+              if(this.value==""){return;}
+              $.get('/users/create/' + this.value, function(data) {
+                $.each(data,function(key, value) {
+                  $("#n_idsede").append('<option value='+value.n_idsede+'>'+value.t_sede+'</option>');
+                });
+                @if ($users->sede!=null)
+                  $("#n_idsede").val({{ $users->sede->n_idsede }});
+                @endif            
+
+              });
+        });        
+        @if ($users->sede!=null && $users->sede->ciudad!=null ) 
+          //console.log('tiene Sesde y ciudad');
+          $('#n_idciudad').val({{ $users->sede->ciudad->n_id  }})
+          $("#n_idciudad").trigger('change');
+          //setTimeout(function(){ 
+            //console.log('Coloco la sede')  
+            //$("#n_idsede").val({{ $users->sede->n_idsede }});
+          //}, 5000);          
+        @endif
+     });     
+     
   </script>
 @endsection
